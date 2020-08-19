510277a1cd954a2a53de284f7fe643dc4efbabe4
ltcoverwatch <litianchi0101@gmail.com>
2020-08-19 20:34:01 +0900

    添加两个拦截器的重写方法，没有实现

--- src/main/java/com/percent/filtertry01/component/MyHandlerInterceptor.java
+++ src/main/java/com/percent/filtertry01/component/MyHandlerInterceptor.java
@@ -1,6 +1,7 @@
 package com.percent.filtertry01.component;
 
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,5 +12,15 @@ public class MyHandlerInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         request.getRequestDispatcher("/index").forward(request, response);
         return false;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
     }
 }
